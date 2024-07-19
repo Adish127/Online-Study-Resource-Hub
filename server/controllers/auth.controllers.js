1a2a4ed3ee0b909b51158172de95fb514c5c6ad5
Hemanth-5 <22z225@psgtech.ac.in>
2024-07-19 21:21:07 +0530

    added auth routes

---
+++ server/controllers/auth.controllers.js
@@ -0,0 +1,59 @@
+import jwt from "jsonwebtoken";
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const register = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const user = new User({ email, password: hashedPassword });
+
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.status(200).json({
+      token,
+      user: {
+        id: user._id,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { login, register };
--- server/index.js
+++ server/index.js
@@ -4,13 +4,20 @@ import cors from "cors";
 
 import connectDB from "./config/db.js";
 
+import authRoutes from "./routes/auth.routes.js";
+
 dotenv.config();
 
+// App Config
 const app = express();
 
+// Inbuilt Middleware
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
+
+// API Routes
+app.use("/api/auth", authRoutes);
 
 connectDB(process.env.MONGO_URI)
   .then(() => {
